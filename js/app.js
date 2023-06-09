e6bcf2efcd076bfc6450d5e4680ef82180dcc691
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-06-08 19:44:17 -0500

    taller 14

--- js/app.js
+++ js/app.js
@@ -2,12 +2,35 @@ console.log('Componente VUE')
 console.log(Vue)
 /*con esto se escriben todos los componentes de vue */
 
-const app=Vue.createApp({
+const app = Vue.createApp({
     //`` estas comillas son backtips
-    template: `
-     <h1> Hola mundo</h1>
-     <p>Desde Vue.JS</p>   
-    `
+    //template: `
+    //   <h1> Hola mundo</h1>
+    //   <p>Desde Vue.JS</p>
+    //   <p>{{1+1}}</p> 
+    //  `
+
+    data() {
+        return {
+            nombre: 'Pablo',
+            apellido: 'Suntaxi',
+            mensaje: 'Hola mundo 2 desde Vue.JS'
+
+        }
+    },
+    //OPCION DE METODOS(me permite declarar metodos en el JS)
+    methods: {
+        cambiarNombre() {
+            console.log('cambiar nombre')
+            this.nombre = 'Daniel'
+
+        },
+        cambiarApellido() {
+            console.log('cambiar apellido')
+            this.apellido = 'Teran'
+
+        }
+    }
 })
 /*se crea un identificador */
 app.mount('#myApp')
